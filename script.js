5d3e0bb08263ac91e43623f30822aa54eec6ec47
StephenTurk90 <stephenjturk@gmail.com>
2022-07-28 15:12:37 -0400

    add all numbers to console.log, change additional code into EXTRACODE.TXT

--- script.js
+++ script.js
@@ -1,52 +1,61 @@
 console.log("hello")
 
+let zeroButton = document.querySelector('.number0');
 let oneButton = document.querySelector('.number1');
+let twoButton = document.querySelector('.number2');
+let threeButton = document.querySelector('.number3');
+let fourButton = document.querySelector('.number4');
+let fiveButton = document.querySelector('.number5');
+let sixButton = document.querySelector('.number6');
+let sevenButton = document.querySelector('.number7');
+let eightButton = document.querySelector('.number8');
+let nineButton = document.querySelector('.number9');
+
+zeroButton.addEventListener('click', function() {
+    console.log('0')
+})
+// 0 button console log
 
 oneButton.addEventListener('click', function() {
     console.log('1')
 })
+// 1 button console log
 
+twoButton.addEventListener('click', function() {
+    console.log('2')
+})
+// 2 button console log
 
+threeButton.addEventListener('click', function() {
+    console.log('3')
+})
+// 3 button console log
 
-let sevenButton = document.querySelector('.number7');
+fourButton.addEventListener('click', function() {
+    console.log('4')
+})
+// 4 button console log
+
+fiveButton.addEventListener('click', function() {
+    console.log('5')
+})
+// 5 button console log
+
+sixButton.addEventListener('click', function() {
+    console.log('6')
+})
+// 6 button console log
 
 sevenButton.addEventListener('click', function() {
     console.log('7')
 })
+// 7 button console log
 
+eightButton.addEventListener('click', function() {
+    console.log('8')
+})
+// 8 button console log
 
-
-// var calculator = document.querySelector('.calculator')
-// var keys = calculator.querySelector('.calculator-keys')
-// var action = keys.dataset.action
-
-// keys.addEventListener('click', function(){
-//     if (keys.matches('button')) {
-//         // Do something
-//         if(action) {
-//             console.log('number key!')
-//         }
-
-//         if (
-//             action === "add "||
-//             action === "substract" ||
-//             action === "multiply" ||
-//             action === "divide"
-//         ) {
-//             console.log('operator key!')
-//         }
-
-//         if (action === 'decimal') {
-//             console.log('decimal key!')
-//         }
-
-//         if (action === 'clear') {
-//             console.log('clear key!')
-//         }
-
-//         if (action === 'calculate') {
-//             console.log('equal key!')
-//         }
-//     }
-// }
-// )  
+nineButton.addEventListener('click', function(){
+    console.log('9')
+})
