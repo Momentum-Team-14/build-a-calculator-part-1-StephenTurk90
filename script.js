edbb2aa94ad165421479bfb539777442167945ec
StephenTurk90 <stephenjturk@gmail.com>
2022-07-28 14:42:24 -0400

    blank out code, add console log presses for 1 and 7, added classes for each individual number button

--- script.js
+++ script.js
@@ -1,34 +1,52 @@
-const calculator = document.querySelector('.calculator')
-const keys = calculator.querySelector('.calculator-keys')
-const action = keys.dataset.action
+console.log("hello")
 
-keys.addEventListener('click', function(){
-    if (keys.matches('button')) {
-        // Do something
-        if(!action) {
-            console.log('number key!')
-        }
+let oneButton = document.querySelector('.number1');
 
-        if (
-            action === "add "||
-            action === "substract" ||
-            action === "multiply" ||
-            action === "divide"
-        ) {
-            console.log('operator key!')
-        }
+oneButton.addEventListener('click', function() {
+    console.log('1')
+})
 
-        if (action === 'decimal') {
-            console.log('decimal key!')
-        }
 
-        if (action === 'clear') {
-            console.log('clear key!')
-        }
 
-        if (action === 'calculate') {
-            console.log('equal key!')
-        }
-    }
-}
-)  
+let sevenButton = document.querySelector('.number7');
+
+sevenButton.addEventListener('click', function() {
+    console.log('7')
+})
+
+
+
+// var calculator = document.querySelector('.calculator')
+// var keys = calculator.querySelector('.calculator-keys')
+// var action = keys.dataset.action
+
+// keys.addEventListener('click', function(){
+//     if (keys.matches('button')) {
+//         // Do something
+//         if(action) {
+//             console.log('number key!')
+//         }
+
+//         if (
+//             action === "add "||
+//             action === "substract" ||
+//             action === "multiply" ||
+//             action === "divide"
+//         ) {
+//             console.log('operator key!')
+//         }
+
+//         if (action === 'decimal') {
+//             console.log('decimal key!')
+//         }
+
+//         if (action === 'clear') {
+//             console.log('clear key!')
+//         }
+
+//         if (action === 'calculate') {
+//             console.log('equal key!')
+//         }
+//     }
+// }
+// )  
